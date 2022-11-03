f24dae74920720b15cca240891037bf80ae9a0b0
Rajnikant Yadav <47982994+rajnikant1698@users.noreply.github.com>
2022-11-03 10:34:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = " Rajnikant Yadav  ";
+
+const myImage = document.querySelector("img");
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+  
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
+  
+  function setUserName() {
+    const myName = prompt("  please enter your name .");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  
